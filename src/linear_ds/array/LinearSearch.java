70da01bd81e141cb8e3c601fac3ad5f60d72cb33
aayush <hiaayush30@gmail.com>
2025-12-31 02:48:50 +0530

    linear search

--- src/linear_ds/array/LinearSearch.java
+++ src/linear_ds/array/LinearSearch.java
@@ -1,22 +1,30 @@
 package linear_ds.array;
 
 public class LinearSearch {
+
+    // O(n) time complexity
+    // O(1) space complexity
+
+    public static int linearSearch (int[] arr, int key){
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==key) {
+                System.out.println("target found at index " + i);
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int [] arr = new int[20];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (Math.random()*100);
         }
-        int target = 15;
 
         for(int i:arr){
             System.out.println(i);
         }
 
-        // linear search
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i]==target){
-                System.out.println("target found at index "+ i);
-            }
-        }
+        linearSearch(arr,15);
     }
 }
